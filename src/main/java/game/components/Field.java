f3818c92e8c831ff6e4c89c305704776038e57be
evanfranciszok <git@evan.mailtje.net>
2024-04-12 11:25:19 +0200

    add extra checks for functions

--- src/main/java/game/components/Field.java
+++ src/main/java/game/components/Field.java
@@ -18,18 +18,21 @@ public class Field {
      * The constructor
      *
      * @param factory_count (int) The amount of factories on the field
-     * @throws InvalidParameterException When the amount of factories is less than 1 or more than 9
+     * @throws InvalidParameterException When the amount of factories is less than 1
+     *                                   or more than 9
      */
-    //@ requires factory_count >= 1 && factory_count <= 9;
-    //@ ensures factories.size() == factory_count;
+    // @ requires factory_count >= 1 && factory_count <= 9;
+    // @ ensures factories.size() == factory_count;
     public Field(int factory_count) throws InvalidParameterException {
         if (factory_count < 1 || factory_count > 9) {
-            throw new InvalidParameterException("field needs at least 1 and at most 9 factories");
+            throw new InvalidParameterException("Field must have between 1 and 9 factories");
         }
         center = TokenPoolInterface.construct("Center");
         factories = new ArrayList<>(factory_count);
+        // @ loop_invariant factories.size() == i && i >= 0 && i <= factory_count;
+        // @ decreases factory_count - i;
         for (int i = 0; i < factory_count; i++) {
-            factories.add(TokenPoolInterface.construct("Factory " + (1 + i)));
+            factories.add(TokenPoolInterface.construct("Factory " + (i + 1)));
         }
     }
 
@@ -38,7 +41,7 @@ public class Field {
      *
      * @return The center
      */
-    //@ ensures \result == center;
+    // @ ensures \result == center;
     public TokenPoolInterface getCenter() {
         return center;
     }
@@ -49,11 +52,28 @@ public class Field {
      * @param bag (Bag) The bag from which the tiles can be grabbed
      * @return Whether enough tiles were available to fill the factories
      */
-    //@ ensures (\forall TokenPoolInterface factory; factories.contains(factory); factory.getContents().size() == 4);
+    // @ ensures (\forall TokenPoolInterface factory; factories.contains(factory);
+    // factory.getContents().size() == 4);
+    // @ ensures (\forall TokenPoolInterface factory; factories.contains(factory);
+    // @ factory.getContents().size() == \old(factory.getContents().size()) + (4 -
+    // \old(factory.getContents().size())));
+    // @ ensures (\forall Tile t; \old(bag.contains(t)); (\exists TokenPoolInterface
+    // factory; factories.contains(factory);
+    // @ factory.getContents().contains(t)));
+    // @ ensures \result == (\forall TokenPoolInterface factory;
+    // factories.contains(factory);
+    // @ factory.getContents().size() == 4);
     public boolean fillFactoriesFromBag(Bag bag) {
         if (center.isEmpty()) {
             center.addTile(Tile.STARTING_PLAYER_TILE);
         }
+        // @ loop_invariant bag.contains(Tile.STARTING_PLAYER_TILE) ==
+        // \old(bag.contains(Tile.STARTING_PLAYER_TILE));
+        // @ loop_invariant (\forall TokenPoolInterface factory;
+        // factories.contains(factory);
+        // @ factory.getContents().size() == \old(factory.getContents().size()) + (4 -
+        // \old(factory.getContents().size())));
+        // @ decreases bag.size();
         for (TokenPoolInterface factory : factories) {
             while (factory.getContents().size() < 4) {
                 if (bag.isEmpty()) {
@@ -70,18 +90,22 @@ public class Field {
      *
      * @return The factories
      */
-    //@ ensures \result.equals(factories);
+    // @ ensures \result.equals(factories);
     public List<TokenPoolInterface> getFactories() {
         return factories;
     }
 
     /**
-     * Getter for the not empty factories
+     * Getter for the non-empty factories
      *
-     * @return all not empty factories
+     * @return All non-empty factories
      */
-    //@ ensures \result.size() == (\old(factories.stream().filter(factory -> !factory.isEmpty()).toList()).size());
+    // @ ensures \result.equals(\old(factories.stream().filter(factory ->
+    // !factory.isEmpty()).toList()));
+    // @ ensures \result.size() == (\old(factories.stream().filter(factory ->
+    // !factory.isEmpty()).toList()).size());
     public List<TokenPoolInterface> getNonEmptyFactories() {
         return new ArrayList<>(factories.stream().filter(factory -> !factory.isEmpty()).toList());
     }
+
 }
